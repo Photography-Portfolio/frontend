bcc969966c513c16d1234a888f773765dfff0efe
Mitchell Mullen <andrew.mullen@avast.com>
2021-02-03 12:31:54 +0100

    nav burger works

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,13 +5,13 @@
           <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-black" href="/">
             Mullen Photography
           </a>
-          <button class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden md:hidden outline-none focus:outline-none" type="button">
+          <button v-on:click="burger()" class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none" type="button">
             <span class="block relative w-6 h-px rounded-sm bg-black"></span>
             <span class="block relative w-6 h-px rounded-sm bg-black mt-1"></span>
             <span class="block relative w-6 h-px rounded-sm bg-black mt-1"></span>
           </button>
         </div>
-        <div class="lg:flex md:flex flex-grow items-center" id="example-navbar-warning">
+        <div class="lg:flex lg:flex-grow items-center px-1" id="example-navbar-warning" v-bind:class="{'hidden': !showNav, 'flex': showNav}">
           <ul class="flex flex-col lg:flex-row md:flex-row list-none ml-auto">
               <li class="nav-item">
                 <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75" href="/discover">
@@ -37,5 +37,15 @@
 <script>
 export default {
   name: "Home",
+  data() {
+		return {
+			showNav: false,
+		};
+  },
+  methods: {
+    burger: function (event) {
+      this.showNav = !this.showNav;
+    }
+  },
 };
 </script>
