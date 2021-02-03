52e118b1d6d0aabe335ef8d31babf559ccc043b7
Mitchell Mullen <andrew.mullen@avast.com>
2021-02-03 21:03:42 +0100

    successful gird design

--- src/views/Discover.vue
+++ src/views/Discover.vue
@@ -1,41 +1,27 @@
 <template>
-  <div class="flex flex-wrap -mx-6 overflow-hidden sm:-mx-5 md:-mx-6 lg:-mx-6 xl:-mx-8 justify-center w-100">
-
-    <div v-for="index in 10" :key="index" class="my-6 px-6 w-full overflow-hidden sm:my-5 sm:px-5 md:my-6 md:px-6 md:w-1/2 lg:my-6 lg:px-6 lg:w-1/3 xl:my-8 xl:px-8 xl:w-1/3">
-      <div class="w-12/12 px-4">
-        <img src="../assets/pictures/img_31.jpeg" />
-      </div>
-    </div>
-
-    <div class="my-6 px-6 w-full overflow-hidden sm:my-5 sm:px-5 md:my-6 md:px-6 md:w-1/2 lg:my-6 lg:px-6 lg:w-1/3 xl:my-8 xl:px-8 xl:w-1/3">
-      <!-- Column Content -->
-    </div>
-
-    <div class="my-6 px-6 w-full overflow-hidden sm:my-5 sm:px-5 md:my-6 md:px-6 md:w-1/2 lg:my-6 lg:px-6 lg:w-1/3 xl:my-8 xl:px-8 xl:w-1/3">
-      <!-- Column Content -->
-    </div>
-
-    <div class="my-6 px-6 w-full overflow-hidden sm:my-5 sm:px-5 md:my-6 md:px-6 md:w-1/2 lg:my-6 lg:px-6 lg:w-1/3 xl:my-8 xl:px-8 xl:w-1/3">
-      <!-- Column Content -->
-    </div>
-
-    <div class="my-6 px-6 w-full overflow-hidden sm:my-5 sm:px-5 md:my-6 md:px-6 md:w-1/2 lg:my-6 lg:px-6 lg:w-1/3 xl:my-8 xl:px-8 xl:w-1/3">
-      <!-- Column Content -->
-    </div>
-
-    <div class="my-6 px-6 w-full overflow-hidden sm:my-5 sm:px-5 md:my-6 md:px-6 md:w-1/2 lg:my-6 lg:px-6 lg:w-1/3 xl:my-8 xl:px-8 xl:w-1/3">
-      <!-- Column Content -->
-    </div>
-
-  </div>
+	<div
+		class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-transparent z-40"
+	>
+		<div
+			class="container px-4 mx-auto flex flex-wrap items-center justify-between lg:px-40"
+		>
+			<div class="w-full relative flex justify-center px-4">
+				<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+					  <img class="box-border w-12/12 h-12/12 md:h-80 md:w-80 object-contain" src="../assets/pictures/img_31.jpeg" />
+					  <img class="box-border w-12/12 h-12/12 md:h-80 md:w-80 object-contain" src="../assets/pictures/img_32.jpeg" />
+            <img class="box-border w-12/12 h-12/12 md:h-80 md:w-80 object-contain" src="../assets/pictures/img_08.jpeg" />
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
+import { defineProps, reactive } from "vue";
 
 defineProps({
-  msg: String
-})
+	msg: String,
+});
 
-const state = reactive({ count: 0 })
+const state = reactive({ count: 0 });
 </script>
