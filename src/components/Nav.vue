194d6e3e368ba51d286da5cf791e03ec632f1bc1
Mitchell Mullen <andrew.mullen@avast.com>
2021-02-03 13:10:00 +0100

    fix nav bug

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-transparent z-40">
       <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
-        <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start md:w-auto md:static md:block md:justify-start">
+        <div class="w-full relative flex justify-between px-4 lg:w-auto lg:static lg:block lg:justify-start md:w-auto md:static md:block md:justify-start">
           <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-black" href="/">
             Mullen Photography
           </a>
@@ -11,7 +11,7 @@
             <span class="block relative w-6 h-px rounded-sm bg-black mt-1"></span>
           </button>
         </div>
-        <div class="lg:flex lg:flex-grow items-center px-1" id="example-navbar-warning" v-bind:class="{'hidden': !showNav, 'flex': showNav}">
+        <div class="md:flex md:flex-grow items-center px-1" id="example-navbar-warning" v-bind:class="{'hidden': !showNav, 'flex': showNav}">
           <ul class="flex flex-col lg:flex-row md:flex-row list-none ml-auto">
               <li class="nav-item">
                 <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75" href="/discover">
