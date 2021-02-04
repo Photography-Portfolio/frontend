64218e11997372cb0ef66372c41ae055df560bee
Mitchell Mullen <andrew.mullen@avast.com>
2021-02-04 11:30:37 +0100

    added all links with pages

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,9 +2,9 @@
     <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-transparent z-40">
       <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div class="w-full relative flex justify-between px-4 lg:w-auto lg:static lg:block lg:justify-start md:w-auto md:static md:block md:justify-start">
-          <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-black" href="/">
+          <router-link :to="{name: 'home'}" class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-black">
             Mullen Photography
-          </a>
+          </router-link>
           <button v-on:click="burger()" class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none" type="button">
             <span class="block relative w-6 h-px rounded-sm bg-black"></span>
             <span class="block relative w-6 h-px rounded-sm bg-black mt-1"></span>
@@ -14,19 +14,19 @@
         <div class="md:flex md:flex-grow items-center px-1" id="example-navbar-warning" v-bind:class="{'hidden': !showNav, 'flex': showNav}">
           <ul class="flex flex-col lg:flex-row md:flex-row list-none ml-auto">
               <li class="nav-item">
-                <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75" href="/discover">
+                <router-link to="discover" class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75">
                   Discover
-                </a>
+                </router-link>
               </li>
               <li class="nav-item">
-                <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75" href="/profile">
-                  Profile
-                </a>
+                <router-link to="about" class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75">
+                  About
+                </router-link>
               </li>
               <li class="nav-item">
-                <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75" href="/contact">
+                <router-link to="contact" class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75">
                   Contact
-                </a>
+                </router-link>
               </li>
           </ul>
         </div>
--- src/router.js
+++ src/router.js
@@ -4,16 +4,30 @@ import {
 } from "vue-router";
 import Home from "./views/Home.vue";
 import Discover from './views/Discover.vue';
+import About from './views/About.vue';
+import Contact from './views/Contact.vue';
 
 const history = createWebHistory();
 const routes = [{
         path: "/",
+        name: "home",
         component: Home
     },
     {
         path: "/discover",
+        name: "discover",
         component: Discover
-    }
+    },
+    {
+        path: "/about",
+        name: "about",
+        component: About
+    },
+    {
+        path: "/contact",
+        name: "contact",
+        component: Contact
+    },
 ];
 const router = createRouter({
     history,
