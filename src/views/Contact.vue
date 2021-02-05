07c2224c8d09c254b8c5fe09a7bfa17327e16a4c
Mitchell Mullen <andrew.mullen@avast.com>
2021-02-05 15:07:15 +0100

    about and contact container

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>Contact</h1>
+	<div
+		class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-transparent z-40"
+	>
+		<div
+			class="container px-4 mx-auto flex flex-wrap items-center justify-between lg:px-40"
+		>
+			<div class="w-full relative flex justify-left px-4">
+				<h1>Contact</h1>
+			</div>
+		</div>
+	</div>
 </template>
