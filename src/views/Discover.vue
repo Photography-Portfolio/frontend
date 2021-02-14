d2b02197843c6684fec6118c867cb7362990f09b
Mitchell Mullen <michalmullen@gmail.com>
2021-02-14 20:39:31 +0100

    feat: discover image links

--- src/views/Discover.vue
+++ src/views/Discover.vue
@@ -7,29 +7,9 @@
 		>
 			<div class="w-full relative flex justify-center px-4">
 				<div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
-					<img
+					<img v-for="image in images" :key="image"
 						class="box-border w-12/12 h-12/12 lg:h-80 lg:w-80 object-contain"
-						src="../assets/pictures/img_31.jpeg"
-					/>
-					<img
-						class="box-border w-12/12 h-12/12 lg:h-80 lg:w-80 object-contain"
-						src="../assets/pictures/img_32.jpeg"
-					/>
-					<img
-						class="box-border w-12/12 h-12/12 lg:h-80 lg:w-80 object-contain"
-						src="../assets/pictures/img_08.jpeg"
-					/>
-					<img
-						class="box-border w-12/12 h-12/12 lg:h-80 lg:w-80 object-contain"
-						src="../assets/pictures/img_31.jpeg"
-					/>
-					<img
-						class="box-border w-12/12 h-12/12 lg:h-80 lg:w-80 object-contain"
-						src="../assets/pictures/img_32.jpeg"
-					/>
-					<img
-						class="box-border w-12/12 h-12/12 lg:h-80 lg:w-80 object-contain"
-						src="../assets/pictures/img_08.jpeg"
+						v-bind:src="image"
 					/>
 				</div>
 			</div>
@@ -37,12 +17,20 @@
 	</div>
 </template>
 
-<script setup>
-import { defineProps, reactive } from "vue";
-
-defineProps({
-	msg: String,
-});
-
-const state = reactive({ count: 0 });
+<script>
+export default {
+	name: "Discover",
+	data() {
+		return {
+			images: [
+				"src/assets/pictures/img_32.jpeg",
+				"src/assets/pictures/img_31.jpeg",
+				"src/assets/pictures/img_08.jpeg",
+				"src/assets/pictures/img_31.jpeg",
+				"src/assets/pictures/img_08.jpeg",
+				"src/assets/pictures/img_32.jpeg",
+			]
+		};
+	},
+};
 </script>
